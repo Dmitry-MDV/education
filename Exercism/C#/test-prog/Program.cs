f469daa91a35f2915ee176593126268d5004e873
Dmitry-MDV <dmitry_mdv@inbox.ru>
2026-07-30 15:58:32 +0300

    Added a test project "TestProgram" for debugging Exercism's exercises. The project is configured to debug the Exercism's "Squeaky Clean" exercise

---
+++ Exercism/C#/test-prog/Program.cs
@@ -0,0 +1,19 @@
+﻿// using System;
+
+public class Program {
+
+    public static void Main(string[] args) {
+        Identifier.Clean("àḃç");
+        Identifier.Clean("my   Id");
+        Identifier.Clean("my\0Id");
+        Identifier.Clean(string.Empty);
+        Identifier.Clean("à-ḃç");
+        Identifier.Clean("à-Ḃç");
+        Identifier.Clean("abc-def-ghi");
+        Identifier.Clean("My😀😀Finder😀");
+        Identifier.Clean("1My2Finder3");
+        Identifier.Clean("MyΟβιεγτFinder");
+        Identifier.Clean("9 -abcĐ😀ω\0");
+        Identifier.Clean("");
+    }
+}
---
+++ Exercism/C#/test-prog/squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+public static class Identifier {
+    public static string Clean(string identifier) {
+        Console.Write("\"{0}\"", identifier);
+        if (identifier.Equals(String.Empty)) {
+            Console.WriteLine("\t\"{0}\"", identifier);
+            return identifier;
+        }
+        bool previousDash = false; // the previous character was a dash
+        StringBuilder builder = new StringBuilder();
+        foreach (char ch in identifier) {
+            if (ch == ' ') {
+                builder.Append("_");
+            } else if (char.IsControl(ch)) {
+                builder.Append("CTRL");
+            } else if (ch == '-') {
+                previousDash = true;
+            } else if (ch >= 'α' && ch <= 'ω') {
+            } else if (!Char.IsLetter(ch)) {
+            } else if (previousDash) {
+                builder.Append(Char.ToUpper(ch));
+                previousDash = false;
+            } else {
+                builder.Append(ch);
+            }
+        }
+        identifier = builder.ToString();
+        Console.WriteLine("\t\"{0}\"", identifier);
+        return identifier;
+     // return builder.ToString();
+    }
+}
